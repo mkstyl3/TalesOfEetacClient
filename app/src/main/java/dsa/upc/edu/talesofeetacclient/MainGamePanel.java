e45531021577ad7259fd951e5130a6f916b2d1ec
Mike <crack.mike@gmail.com>
2017-12-01 13:32:21 +0100

    tutorial

--- app/src/main/java/dsa/upc/edu/talesofeetacclient/MainGamePanel.java
+++ app/src/main/java/dsa/upc/edu/talesofeetacclient/MainGamePanel.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -15,13 +16,18 @@ import android.view.SurfaceView;
 
 public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
-    private static final String TAG = MainThread.class.getSimpleName();
+    private static final String TAG = MainGamePanel.class.getSimpleName();
+
     private MainThread thread;
+    private Droid droid;
 
     public MainGamePanel(Context context) {
         super(context);
         // adding the callback (this) to the surface holder to intercept events
         getHolder().addCallback(this);
+
+        // create droid and load bitmap
+        droid = new Droid(BitmapFactory.decodeResource(getResources(), R.drawable.link1), 50, 50);
 
         // create the game loop thread
         thread = new MainThread(getHolder(), this);
@@ -31,18 +37,23 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // at this point the surface is created and
+        // we can safely start the game loop
         thread.setRunning(true);
         thread.start();
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d(TAG, "Surface is being destroyed");
+        // tell the thread to shut down and wait for it to finish
+        // this is a clean shutdown
         boolean retry = true;
         while (retry) {
             try {
@@ -52,26 +63,68 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
                 // try again shutting down the thread
             }
         }
+        Log.d(TAG, "Thread was shut down cleanly");
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            // delegating event handling to the droid
+            droid.handleActionDown((int)event.getX(), (int)event.getY());
+
+            // check if in the lower part of the screen we exit
             if (event.getY() > getHeight() - 50) {
                 thread.setRunning(false);
-                ((Activity) getContext()).finish();
+                ((Activity)getContext()).finish();
             } else {
                 Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
             }
+        } if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            // the gestures
+            if (droid.isTouched()) {
+                // the droid was picked up and is being dragged
+                droid.setX((int)event.getX());
+                droid.setY((int)event.getY());
+            }
+        } if (event.getAction() == MotionEvent.ACTION_UP) {
+            // touch was released
+            if (droid.isTouched()) {
+                droid.setTouched(false);
+            }
         }
-
-        return super.onTouchEvent(event);
+        return true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
-                R.drawable.link1), 10, 10, null);
+        // fills the canvas with black
+        canvas.drawColor(Color.BLACK);
+        droid.draw(canvas);
+    }
+
+    public void update() {
+        // check collision with right wall if heading right
+        if (droid.getSpeed().getxDirection() == Speed.DIRECTION_RIGHT
+                && droid.getX() + droid.getBitmap().getWidth() / 2 >= getWidth()) {
+            droid.getSpeed().toggleXDirection();
+        }
+        // check collision with left wall if heading left
+        if (droid.getSpeed().getxDirection() == Speed.DIRECTION_LEFT
+                && droid.getX() - droid.getBitmap().getWidth() / 2 <= 0) {
+            droid.getSpeed().toggleXDirection();
+        }
+        // check collision with bottom wall if heading down
+        if (droid.getSpeed().getyDirection() == Speed.DIRECTION_DOWN
+                && droid.getY() + droid.getBitmap().getHeight() / 2 >= getHeight()) {
+            droid.getSpeed().toggleYDirection();
+        }
+        // check collision with top wall if heading up
+        if (droid.getSpeed().getyDirection() == Speed.DIRECTION_UP
+                && droid.getY() - droid.getBitmap().getHeight() / 2 <= 0) {
+            droid.getSpeed().toggleYDirection();
+        }
+        // Update the lone droid
+        droid.update();
     }
 
 }
--- app/src/main/java/dsa/upc/edu/talesofeetacclient/MainThread.java
+++ app/src/main/java/dsa/upc/edu/talesofeetacclient/MainThread.java
@@ -15,6 +15,12 @@ public class MainThread extends Thread {
     private boolean running;
     private SurfaceHolder surfaceHolder;
     private MainGamePanel gamePanel;
+    // desired fps
+    private final static int 	MAX_FPS = 50;
+    // maximum number of frames to be skipped
+    private final static int	MAX_FRAME_SKIPS = 5;
+    // the frame period
+    private final static int	FRAME_PERIOD = 1000 / MAX_FPS;
 
     public MainThread(SurfaceHolder surfaceHolder, MainGamePanel gamePanel) {
         super();
@@ -31,24 +37,57 @@ public class MainThread extends Thread {
     public void run() {
         Canvas canvas;
         Log.d(TAG, "Starting game loop");
-        while(running) {
+        long beginTime;		// the time when the cycle begun
+        long timeDiff;		// the time it took for the cycle to execute
+        int sleepTime;		// ms to sleep (<0 if we're behind)
+        int framesSkipped;	// number of frames being skipped
+
+        sleepTime = 0;
+        while (running) {
             canvas = null;
-            // try locking the canvas for exclusive pixel editing on the surface
+            // try locking the canvas for exclusive pixel editing
+            // in the surface
             try {
                 canvas = this.surfaceHolder.lockCanvas();
                 synchronized (surfaceHolder) {
+                    beginTime = System.currentTimeMillis();
+                    framesSkipped = 0;	// resetting the frames skipped
                     // update game state
+                    this.gamePanel.update();
+                    // render state to the screen
                     // draws the canvas on the panel
-                    this.gamePanel.onDraw(canvas);
+                    this.gamePanel.render(canvas);
+                    // calculate how long did the cycle take
+                    timeDiff = System.currentTimeMillis() - beginTime;
+                    // calculate sleep time
+                    sleepTime = (int)(FRAME_PERIOD - timeDiff);
+
+                    if (sleepTime > 0) {
+                        // if sleepTime > 0 we're OK
+                        try {
+                            // send the thread to sleep for a short period
+                            // very useful for battery saving
+                            Thread.sleep(sleepTime);
+                        } catch (InterruptedException e) {}
+                    }
+
+                    while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
+                        // we need to catch up
+                        // update without rendering
+                        this.gamePanel.update();
+                        // add frame period to check if in next frame
+                        sleepTime += FRAME_PERIOD;
+                        framesSkipped++;
+                    }
                 }
-            }
-            finally {
+            } finally {
                 // in case of an exception the surface is not left in
                 // an inconsistent state
                 if (canvas != null) {
                     surfaceHolder.unlockCanvasAndPost(canvas);
                 }
-            }
+            }	// end finally
         }
     }
 }
+
