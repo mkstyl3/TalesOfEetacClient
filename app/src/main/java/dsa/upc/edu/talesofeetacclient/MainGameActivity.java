9dee3faa096ecc399f969d1a961b056226bcd69e
IfJosean <josean7jkami@gmail.com>
2018-01-20 17:30:16 +0100

    music when play

--- app/src/main/java/dsa/upc/edu/talesofeetacclient/MainGameActivity.java
+++ app/src/main/java/dsa/upc/edu/talesofeetacclient/MainGameActivity.java
@@ -2,6 +2,7 @@ package dsa.upc.edu.talesofeetacclient;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import dsa.upc.edu.talesofeetacclient.Model.Main.User;
@@ -12,6 +13,7 @@ public class MainGameActivity extends Activity {
     // and respond to screen touches as well
     GameView gameView;
     User user;
+    MediaPlayer mySong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,8 @@ public class MainGameActivity extends Activity {
         gameView = new GameView(this,user);
         setContentView(gameView);
 
+        mySong=MediaPlayer.create(MainGameActivity.this,R.raw.plateau);
+
     }
     // This method executes when the player starts the game
     @Override
@@ -29,6 +33,8 @@ public class MainGameActivity extends Activity {
         super.onResume();
         // Tell the gameView resume method to execute
         gameView.resume();
+
+        mySong.start();
     }
     // This method executes when the player quits the game
     @Override
